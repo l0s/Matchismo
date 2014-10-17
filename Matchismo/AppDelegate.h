3d8925e142fe148db668908fcc718539f43325d6
Carlos Macasaet <github@carlos-m.net>
2014-10-16 23:09:41 -0700

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Carlos Macasaet on 10/16/14.
+//  Copyright (c) 2014 Carlos Macasaet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Carlos Macasaet on 10/16/14.
+//  Copyright (c) 2014 Carlos Macasaet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
